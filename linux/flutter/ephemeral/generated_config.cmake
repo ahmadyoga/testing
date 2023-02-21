30b39369e85c830921ef952443a4bb398c782108
ahmadyoga <ahmadyoga684@gmail.com>
2023-02-21 18:33:20 +0700

    add file 2

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/ahmadyoga/Documents/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/ahmadyoga/Documents/project/flutter_app_coba" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/ahmadyoga/Documents/flutter"
+  "PROJECT_DIR=/home/ahmadyoga/Documents/project/flutter_app_coba"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/ahmadyoga/Documents/project/flutter_app_coba/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/ahmadyoga/Documents/project/flutter_app_coba/lib/main.dart"
+)
